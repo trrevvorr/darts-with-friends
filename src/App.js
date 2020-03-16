54d2150a071f11ed36553e215e879e63ca96eea6
Trevor Ross <theboss@amazon.com>
2020-03-15 21:39:27 -0400

    added material-ui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'typeface-roboto';
+import Button from '@material-ui/core/Button';
+import Helmet from "react-helmet";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="application">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Darts With Friends</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Helmet>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
     </div>
   );
 }
